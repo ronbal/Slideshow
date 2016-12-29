539ea3f5f1c11e1b4f5de6d8cda329bf7e318541
Hatschi915 <c.hartmann24@googlemail.com>
2016-12-29 23:42:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ WorkingDir = os.getcwd()
 #check_call(['sudo', 'apt-get', 'install', 'samba', 'samba-common-bin', '-y'])
 #subprocess.call("(sudo cp /home/pi/temp/bin/smb.conf /etc/samba/samba.conf)", shell=True)
 #subprocess.call("(sudo /etc/init.d/samba restart)", shell=True)
-subprocess.call("(sudo apt-get install python3-dev python3-setuptools libjpeg-dev zlib1g-dev libpng12-dev libfreetyp$
+subprocess.call("(sudo apt-get install python3-dev python3-setuptools libjpeg-dev zlib1g-dev libpng12-dev libfreetype6-dev)", shell=True)
 subprocess.call("(sudo apt-get install python3-pip)", shell=True)
 subprocess.call("(sudo pip-3.2 install pi3d)", shell=True)
 subprocess.call("(sudo pip-3.2 install Pillow)", shell=True)
 subprocess.call("(sudo pip install pi3d)", shell=True)
-subprocess.call("(sudo cp /home/pi/temp/app/shaders/*.*  /usr/local/lib/python2.7/dist-packages/pi3d/shaders)", shel$
+subprocess.call("(sudo cp /home/pi/temp/app/shaders/*.*  /usr/local/lib/python2.7/dist-packages/pi3d/shaders)", shell=True)
 subprocess.call("(sudo pip install pyinotify)", shell=True)
 
 subprocess.call("(sudo mv /home/pi/temp/Advertising /home/pi/Advertising_temp)", shell=True)
