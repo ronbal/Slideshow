8acdce878af583216032f6eb107bb5e2902bf6bf
Hatschi915 <c.hartmann24@googlemail.com>
2016-12-29 22:45:39 +0100

    Update setup.py

--- bin/setup.py
+++ bin/setup.py
@@ -1,5 +1,7 @@
 from subprocess import STDOUT, check_call
 import os
 
-check_call(['sudo', 'apt-get', 'update', '-y'])
-check_call(['sudo', 'apt-get', 'upgrade', '-y']) 
+#check_call(['sudo', 'apt-get', 'update', '-y'])
+#check_call(['sudo', 'apt-get', 'upgrade', '-y']) 
+
+subprocess.call("(cd ~/temp/slideshow/)", shell=True)
