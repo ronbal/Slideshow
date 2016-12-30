d2cbbb6f847daa4727d32d5fb188d0be3159dca3
Hatschi915 <c.hartmann24@googlemail.com>
2016-12-30 01:11:35 +0100

    Update Hotfolder.py

--- app/Hotfolder.py
+++ app/Hotfolder.py
@@ -1,1 +1,57 @@
+import subprocess
+import shutil
+import pyinotify
+import time
+import glob
+import os
+
+    
+
+class MyEventHandler(pyinotify.ProcessEvent):
+    def process_IN_CLOSE_WRITE(self, event):
+        print "CLOSE_WRITE event:", event.pathname
+        head,tail = os.path.split(event.pathname)
+        if not event.pathname.endswith((".jpg", "JPG", "jpeg", "JPEG")): 
+            print ("Skipping:" +str(event.pathname))
+        else:
+            try:
+                if not glob.glob("/media/usb0/.donotdelete.txt"):
+                    shutil.copyfile(event.pathname,"/media/usb0/"+str(tail))
+                    print "created: /media/usb/"+str(tail)
+                else:
+                    print "USB-Stick not present"
+            except:
+                print "FAILED creation: /media/usb0/"+str(tail)+" - USB-Stick not present"
+                pass
+            try:
+                if not glob.glob("/home/pi/app/temp.jpg"):
+                    shutil.copyfile(event.pathname,"/home/pi/app/temp.jpg")
+                    print "created: /home/pi/app/temp.jpg"
+                else:
+                    print "creation of temp.jpg skipped"
+            except:
+                print "FAILED creation /home/pi/app/temp.jpg"
+                pass
+            try:
+                subprocess.call(["sudo", "rm", event.pathname])
+                print "Removed: "+event.pathname
+            except Exception as e:
+                print str(e)
+                pass
+
+def main():
+    # watch manager
+    wm = pyinotify.WatchManager()
+    wm.add_watch('/home/pi/slideshow', pyinotify.ALL_EVENTS, rec=True)
+
+    # event handler
+    eh = MyEventHandler()
+
+    # notifier
+    notifier = pyinotify.Notifier(wm, eh)
+    notifier.loop()
+
+if __name__ == '__main__':
+    main()
+
 
